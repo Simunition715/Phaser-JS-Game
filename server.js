6502aef103ee52549311f281b04c52ecce95b498
William Blackwell <williamblackwell@Williams-MacBook-Air.local>
2017-04-12 13:36:00 -0500

    js game

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+
+var app = express();
+
+app.use(express.static(__dirname + '/client'));
+
+
+app.use(express.static(__dirname + '/bower_components'));
+require('./server/config/mongoose.js');
+// require('./server/config/routes.js')(app);
+
+app.listen(8000, function(){
+	console.log('Listening on port 8000...');
+})
